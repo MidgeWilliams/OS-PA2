69e88a676c2eadba6bf651e32d3d9d03a75049f9
MidgeWilliams <43554894+MidgeWilliams@users.noreply.github.com>
2018-11-02 16:29:27 -0400

    UpdatePriority

--- src/cs131/pa2/CarsTunnels/PriorityScheduler.java
+++ src/cs131/pa2/CarsTunnels/PriorityScheduler.java
@@ -1,14 +1,34 @@
 package cs131.pa2.CarsTunnels;
 
-import cs131.pa2.Abstract.Tunnel;
-import cs131.pa2.Abstract.Vehicle;
+import java.util.*;
+
+import cs131.pa2.Abstract.*;
+import cs131.pa2.Abstract.Log.*;
 
 public class PriorityScheduler extends Tunnel{
-
+	
+	private Collection<Tunnel> tunnels;
+	private ArrayList<ArrayList<Vehicle>> priority;
+	
 	public PriorityScheduler(String name) {
 		super(name);
+		tunnels = new ArrayList<>();
+		makePriorityLists();
 	}
-
+	
+	public PriorityScheduler(String name, Collection<Tunnel> tunnels, Log log) {
+		super(name, log);
+		this.tunnels = tunnels;
+		makePriorityLists();
+	}
+	
+	private void makePriorityLists() {
+		priority = new ArrayList<ArrayList<Vehicle>>();
+		for(int i = 0; i < 5; i++) {
+			priority.add(new ArrayList<>());
+		}
+	}
+	
 	@Override
 	public boolean tryToEnterInner(Vehicle vehicle) {
 		return false;
