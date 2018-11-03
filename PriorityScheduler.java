99f886e7d026fab289a9bc19967f6b8f7f42d0d2
Siegeerson <ben.siege@gmail.com>
2018-11-03 17:58:04 -0400

    finished priority

--- PriorityScheduler.java
+++ PriorityScheduler.java
@@ -7,21 +7,23 @@ import java.util.concurrent.locks.ReentrantLock;
 import cs131.pa2.Abstract.*;
 import cs131.pa2.Abstract.Log.*;
 
-public class PriorityScheduler extends Tunnel{
+public class PriorityScheduler extends Tunnel {
 	private final Lock lock = new ReentrantLock();
 	private final Condition exitedFromTunnel = lock.newCondition();
 	private Collection<Tunnel> tunnels;
 	private ArrayList<ArrayList<Vehicle>> priority;
-	
+	private HashMap<Vehicle, Tunnel> vehicleMap;
+
 	/**
 	 * @param name
 	 */
 	public PriorityScheduler(String name) {
 		super(name);
 		tunnels = new ArrayList<>();
+		vehicleMap = new HashMap<>();
 		makePriorityLists();
 	}
-	
+
 	/**
 	 * @param name
 	 * @param tunnels
@@ -30,74 +32,85 @@ public class PriorityScheduler extends Tunnel{
 	public PriorityScheduler(String name, Collection<Tunnel> tunnels, Log log) {
 		super(name, log);
 		this.tunnels = tunnels;
-		makePriorityLists();//set up priority list
+		vehicleMap = new HashMap<>();
+		makePriorityLists();// set up priority list
 	}
-	
+
 	private void makePriorityLists() {
 		priority = new ArrayList<ArrayList<Vehicle>>();
-		for(int i = 0; i < 5; i++) {
+		for (int i = 0; i < 5; i++) {
 			priority.add(new ArrayList<>());
 		}
 	}
-	
+
 	@Override
 	public boolean tryToEnterInner(Vehicle vehicle) {
-		while(true) {
+		while (true) {
+			lock.lock();
 			int currPr = vehicle.getPriority();
-			if(!priority.get(currPr).contains(vehicle)) {
+			if (!priority.get(currPr).contains(vehicle)) {
 				priority.get(currPr).add(vehicle);
 			}
-			if(!checkHighPrior(currPr) || !checkTunnels(vehicle)) {//if its not its turn or if it can't go in
+			while (!checkHighPrior(currPr) || !checkTunnels(vehicle)) {// if its not its turn or if it can't go in
 				try {
 					exitedFromTunnel.await();
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
+					lock.unlock();
 				}
-			}else {//need to put successful entry code in else block, 
-				priority.get(currPr).remove(vehicle);
-				
-				return false;
 			}
-			
+			// need to put successful entry code in else block,
+			priority.get(currPr).remove(vehicle);
+			lock.unlock();
+			return true;
 		}
-		
+
 	}
-	
+
+
 	/**
 	 * breaks loop by returning false if the vehicle entered
+	 * 
 	 * @param vehicle
 	 * @return
 	 */
 	private boolean checkTunnels(Vehicle vehicle) {
-		for(Tunnel tunnel:tunnels) {
-			if(tunnel.tryToEnter(vehicle)) {
-				//add to tunnel map
+		for (Tunnel tunnel : tunnels) {
+			if (vehicleMap == null)
+				System.out.println("null");
+			if (tunnel.tryToEnterInner(vehicle)) {// use try to enter/exit inner
+				// add to tunnel map
+				vehicleMap.put(vehicle, tunnel);// if it enters the tunnel remember which tunnel
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	/**
-	 * returns true if its its turn
-	 * goes from max to before curr
+	 * returns true if its its turn goes from max to before curr
 	 * 
 	 * @param curr
 	 * @return
 	 */
 	private boolean checkHighPrior(int curr) {
-		for(int i = 4; i > curr; i++) {
-			if(!priority.get(i).isEmpty()) {
-				return false;//if not empty return false
+		for (int i = 4; i > curr; i--) {
+			if (!priority.get(i).isEmpty()) {
+				return false;// if not empty return false
 			}
 		}
-		return true;//if all before are true return true
+		return true;// if all before are true return true
 	}
 
 	@Override
 	public void exitTunnelInner(Vehicle vehicle) {
-		
+		lock.lock();
+		Tunnel correctTunnel = vehicleMap.get(vehicle);
+		correctTunnel.exitTunnelInner(vehicle);
+		vehicleMap.remove(vehicle);
+		exitedFromTunnel.signalAll();
+		lock.unlock();
 	}
-	
+
 }
